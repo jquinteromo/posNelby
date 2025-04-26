669ee86466c3cd987ffb4596e61ef547221490b3
Jaider Quintero <qjaider1111@gmail.com>
2025-04-26 16:54:09 -0500

    Change Server

--- src/server/index.ts
+++ src/server/index.ts
@@ -44,7 +44,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(3001, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
