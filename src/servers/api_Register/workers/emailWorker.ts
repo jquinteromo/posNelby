199ec81f64f04a14b2b08054a40fa78f43b77cb3
Jaider Quintero <qjaider1111@gmail.com>
2025-05-23 19:38:48 -0500

    change emailqueu delet bull

--- src/servers/api_Register/workers/emailWorker.ts
+++ src/servers/api_Register/workers/emailWorker.ts
@@ -1,24 +1,17 @@
-import { Worker,Queue } from "bullmq";
+import { Worker, Job } from "bullmq";
 import { sendOtpEmail } from "../services/emailService";
 
-export const emailQueue = new Queue("emailQueue", {
-  connection: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    tls: process.env.REDIS_TLS === "true" ? {} : undefined,
-    maxRetriesPerRequest: null,
-  },
-});
-
-export const enqueueEmail = (email: string, otp: string, nameUs: string) => {
-  return emailQueue.add("send_email", { email, otp, nameUs });
+// Tipo explícito de datos del trabajo
+type EmailJobData = {
+  email: string;
+  otp: string;
+  nameUs: string;
 };
 
-const emailWorker = new Worker(
+// Worker que procesa trabajos de la cola "emailQueue"
+const emailWorker = new Worker<EmailJobData>(
   "emailQueue",
-  async (job) => {
+  async (job: Job<EmailJobData>) => {
     const { email, otp, nameUs } = job.data;
     await sendOtpEmail(email, otp, nameUs);
   },
